361cd796f5c627f033de1786b5086bce29dbe84f
Matteo Piccini <gnappo.1@live.it>
2023-03-30 15:06:44 -0700

    done.

--- server/debug.py
+++ server/debug.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from app import app
+from app import db
+from models import Owner, Pet
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    with app.app_context():
+        import ipdb; ipdb.set_trace()
